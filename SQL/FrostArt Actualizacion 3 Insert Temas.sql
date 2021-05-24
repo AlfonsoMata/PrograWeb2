72f79b88e544c4bfc26dd15bd189ee4f1eacb389
MasterNetX09 <61898091+MasterNetX09@users.noreply.github.com>
2021-05-23 22:04:40 -0500

    SQL Actualizacion 3

---
+++ SQL/FrostArt Actualizacion 3 Insert Temas.sql
@@ -0,0 +1,24 @@
+/*
+	Equipo 02
+	Integrantes:
+	1800628 José Arturo Rodríguez Cavazos
+	1803321	Alfonso Mata de Anda
+	1800719	Pablo Andrés Mariles Feregrino
+	1838420 Rene Miramontes Hernandez
+
+
+	Query #4
+		Insert de datos necesarios a la table Temas para el funcionamiento de la pagina web
+					
+					
+		Fecha: 2021-05-23
+
+*/
+
+
+
+
+INSERT INTO Temas VALUES('PixelArt', 'https://firebasestorage.googleapis.com/v0/b/frostarttesta.appspot.com/o/pictures%2FpixelArt.jpg?alt=media&token=c2bd5c01-6032-4c6b-b563-339581567642');
+INSERT INTO Temas VALUES('Edicion', 'https://firebasestorage.googleapis.com/v0/b/frostarttesta.appspot.com/o/pictures%2Fedicion.jpg?alt=media&token=27700ee2-d28c-43d7-b256-fd1c1e5ccd13');
+INSERT INTO Temas VALUES('Fotografia', 'https://firebasestorage.googleapis.com/v0/b/frostarttesta.appspot.com/o/pictures%2Fphotos.jpg?alt=media&token=99a7acd8-9340-4ebd-92dc-930883e44120');
+INSERT INTO Temas VALUES('Digital', 'https://firebasestorage.googleapis.com/v0/b/frostarttesta.appspot.com/o/pictures%2Fdigital.jpg?alt=media&token=4c3a9abd-a13a-41ff-84ab-f9269aee32eb');
